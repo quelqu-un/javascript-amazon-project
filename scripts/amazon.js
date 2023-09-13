a6de35d69e0e29de93f0fa3aece89107c555da89
quelqu-un <quelquun117@gmail.com>
2023-09-13 15:16:47 -0300

    save localstorage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -60,7 +60,7 @@ let productsHtml = ''
     let cartQuantity = 0; 
 
     cart.forEach((cartItem)=>{
-      cartQuantity+= cartItem.quantity;
+      cartQuantity += cartItem.quantity;
     });
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
  };
